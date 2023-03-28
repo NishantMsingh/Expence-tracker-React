369c72a1fbc312d9dca94ade22a5d3923e896f4d
Nishant Kumar <93445529+NishantMsingh@users.noreply.github.com>
2023-03-28 14:38:37 +0530

    added event

--- src/components/Expense/Expenceitem.js
+++ src/components/Expense/Expenceitem.js
@@ -1,13 +1,19 @@
 import "./Expense.css";
 import Expensedate from "./Expensedate";
 import ExpenseDetails from "./ExpenseDetails";
-function Expenseitem(props)
-{ 
+const Expenseitem=(props)=>{ 
+
+    // function clickhandler()
+    // {}
+    const clickhandler=()=>{
+        console.log("Har Har Mahadev");
+    }
     return (
         <div className="bgs">
             <div className="Expense-item">
         <Expensedate date={props.date}></Expensedate>
         <ExpenseDetails price={props.price} title={props.title}></ExpenseDetails>
+        <button className="btn" onClick={()=>clickhandler}>Click title</button>
         </div>
         </div>
     );
